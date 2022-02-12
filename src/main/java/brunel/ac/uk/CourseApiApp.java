dfb9467de579fb9b509d801808d29935d9714cf2
Khankee <1940930@brunel.ac.uk>
2022-02-12 23:46:25 +0000

    Learned and developed Spring boot application

---
+++ src/main/java/brunel/ac/uk/CourseApiApp.java
@@ -0,0 +1,14 @@
+package brunel.ac.uk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+
+@SpringBootApplication
+public class CourseApiApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseApiApp.class, args);
+    }
+}
